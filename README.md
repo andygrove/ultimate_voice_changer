5a4d11c3d066c47e5dcf4c745ed44be585010515
Andy Grove <andygrove73@gmail.com>
2014-10-11 10:51:00 -0600

    initial checkin of sample code

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ultimate_voice_changer
 ======================
 
-Ultimate real time voice change for Arduino using MCP3208 ADC and MCP4921 DAC with SPI communications
+Ultimate real time voice change for Arduino using MCP3208 ADC and MCP4921 DAC with SPI communications.
+
+I've just started this project and nothing works yet! Stay tuned....
---
+++ ultimate_voice_changer.ino
@@ -0,0 +1,167 @@
+const int LDAC = 8;
+const int CS_DAC = 9;
+const int CS_ADC = 10;
+const int SPICLOCK = 13;
+const int DATAOUT = 11;
+const int DATAIN = 12;
+
+int fword;
+int cmd = 0x7000;
+int data;
+int tmp;
+
+#define NUM_SINE_WAVE_POINTS 512
+
+byte sineWave[NUM_SINE_WAVE_POINTS];  
+
+void setup() {
+
+  // set pin directions
+  pinMode(LDAC, OUTPUT);
+  pinMode(SPICLOCK, OUTPUT);
+  pinMode(CS_DAC, OUTPUT);
+  pinMode(CS_ADC, OUTPUT);
+  pinMode(DATAOUT, OUTPUT);
+  pinMode(DATAIN, INPUT);
+
+  // disable both the ADC and DAC
+  digitalWrite(CS_ADC, HIGH);
+  digitalWrite(CS_DAC, HIGH);
+
+  fill_sinewave();
+}
+
+int index = 0;
+
+int read_adc(int channel){
+
+  digitalWrite(SPICLOCK, LOW);
+  int adcvalue = 0;
+
+  byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
+
+  //allow channel selection
+  commandbits|=((channel-1)<<3);
+
+  digitalWrite(CS_ADC,LOW); //Select adc
+  // setup bits to be written
+  for (int i=7; i>=3; i--){
+    digitalWrite(DATAOUT,commandbits&1<<i);
+    //cycle clock
+    digitalWrite(SPICLOCK,HIGH);
+    digitalWrite(SPICLOCK,LOW);    
+  }
+
+    //cycle clock twice to ignore two null bits
+  digitalWrite(SPICLOCK,HIGH);    
+  digitalWrite(SPICLOCK,LOW);
+  digitalWrite(SPICLOCK,HIGH);  
+  digitalWrite(SPICLOCK,LOW);
+
+  //read bits from adc
+  for (int i=11; i>=0; i--){
+    adcvalue+=digitalRead(DATAIN)<<i;
+    //cycle clock
+    digitalWrite(SPICLOCK,HIGH);
+    digitalWrite(SPICLOCK,LOW);
+  }
+  digitalWrite(CS_ADC, HIGH); //turn off device
+
+  // reset DATAOUT .. not needed but makes debugging easier
+  digitalWrite(DATAOUT, LOW);  
+
+  // clock is LOW
+  return adcvalue;
+}
+
+/** Write to DAC */
+void write_dac(int data) {
+
+  // combine command and data bits
+  fword = cmd | data;
+  
+  digitalWrite(SPICLOCK, LOW);
+
+  // chip select
+  digitalWrite(CS_DAC, LOW);
+  
+  // start writing data
+  digitalWrite(LDAC, HIGH);
+
+  for (int i1=0; i1<12; i1++) {
+    tmp = fword & 0x8000;
+    
+    // write bit
+    if (tmp) {
+      digitalWrite(DATAOUT, HIGH);
+    }
+    else {
+      digitalWrite(DATAOUT, LOW);  
+    }
+    // cycle clock
+    digitalWrite(SPICLOCK, HIGH);
+    digitalWrite(SPICLOCK, LOW);
+
+    // shift up one
+    fword = fword << 1;
+  }
+
+  digitalWrite(CS_DAC, HIGH); // end of conversion
+
+  digitalWrite(LDAC, LOW); // writing data to output buffer  
+
+  // reset DATAOUT .. not needed but makes debugging easier
+  digitalWrite(DATAOUT, LOW);  
+}
+
+
+void loop() {
+  if (++index == NUM_SINE_WAVE_POINTS)  {
+    index = 0;
+  }
+  data = sineWave[index] * 5;
+  fword = cmd | data;
+  
+  // chip select
+  digitalWrite(CS_DAC, HIGH);
+  digitalWrite(CS_DAC, LOW);
+  
+  // start writing data
+  digitalWrite(LDAC, HIGH);
+  for (int i1=0; i1<16; i1++) {
+    digitalWrite(SPICLOCK, LOW);
+    tmp = fword & 0x8000;
+    digitalWrite(DATAOUT, LOW);
+    if (tmp) {
+      digitalWrite(DATAOUT, HIGH);
+    }
+    digitalWrite(SPICLOCK, HIGH);
+    fword = fword << 1;
+  }
+  digitalWrite(CS_DAC, HIGH); // end of conversion
+  digitalWrite(LDAC, LOW); // writing data to output buffer
+
+//  delay(10);
+}
+
+void fill_sinewave(){
+  float pi = 3.141592;
+  float dx ;
+  float fd ;
+  float fcnt;
+  dx=2 * pi / NUM_SINE_WAVE_POINTS;   // fill the  byte bufferarry
+  int iw;
+  for (iw = 0; iw < NUM_SINE_WAVE_POINTS; iw++){      // with 50 periods sinewawe
+    fd= 127*sin(fcnt);                // fundamental tone
+    fcnt=fcnt+dx;                     // in the range of 0 to 2xpi  and 1/512 increments
+    int bb=127+fd;                        // add dc offset to sinewawe 
+    sineWave[iw]=bb;                  // write value into array
+    // uncomment this to see the sine wave numbers in the serial monitor
+    /*
+    Serial.print("Sine: ");
+    Serial.println(bb);
+    */
+  }
+}    
+    
+
