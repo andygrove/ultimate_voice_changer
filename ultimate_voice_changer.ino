2c7a830a137974893c54784712322e785252ed18
Andy Grove <andygrove73@gmail.com>
2014-10-11 16:32:32 -0600

    code cleanup

--- ultimate_voice_changer.ino
+++ ultimate_voice_changer.ino
@@ -12,12 +12,13 @@ const int cmd = 0x7000;
 
 const long n = 10000;
 
+int index = 0;
 int fword;
 int data;
 int counter = 0;
 long last_time = 0;
 
-#define NUM_SINE_WAVE_POINTS 256
+#define NUM_SINE_WAVE_POINTS 1024
 
 byte sineWave[NUM_SINE_WAVE_POINTS];  
 
@@ -42,39 +43,32 @@ void setup() {
   fill_sinewave();
 }
 
-int index = 0;
-
+/** Set CLK HIGH then LOW */
 inline void cycle_clock() {
-  PORTB |= B00100000;
-  //digitalWrite(SPICLOCK,HIGH);
-  //delayMicroseconds(10);
-  PORTB &= B11011111;
-  //digitalWrite(SPICLOCK,LOW);    
+  PORTB |= B00100000; //digitalWrite(SPICLOCK,HIGH);
+  PORTB &= B11011111; //digitalWrite(SPICLOCK,LOW);    
 }
 
+/** Read from ADC */
 int read_adc(int channel){
-
-  //digitalWrite(SPICLOCK, LOW);
 
   int adcvalue = 0;
 
-  byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
+  // command bits - start, mode, chn (3), dont care (3)
+  byte commandbits = B11000000; 
 
-  //allow channel selection
+  // allow channel selection
   commandbits|=((channel-1)<<3);
 
   // select ADC
-  PORTB &= B11111011;
-  //digitalWrite(CS_ADC,LOW);
+  PORTB &= B11111011; //digitalWrite(CS_ADC,LOW);
 
   // setup bits to be written
   for (int i=7; i>=3; i--){
     if (commandbits&1<<i) {
-      PORTB |= B00001000;
-      //digitalWrite(DATAOUT,HIGH);
+      PORTB |= B00001000; //digitalWrite(DATAOUT,HIGH);
     } else {
-      PORTB &= B11110111;
-      //digitalWrite(DATAOUT,LOW);
+      PORTB &= B11110111; //digitalWrite(DATAOUT,LOW);
     }
     cycle_clock();
   }
@@ -85,13 +79,11 @@ int read_adc(int channel){
 
   //read bits from adc
   for (int i=11; i>=0; i--){
-    adcvalue += ((PINB & B00010000) >> 4) << i;
-    //adcvalue += digitalRead(DATAIN)<<i;
+    adcvalue += ((PINB & B00010000) >> 4) << i; //digitalRead(DATAIN)<<i;
     cycle_clock();
   }
 
-  PORTB |= B00000100;
-  //digitalWrite(CS_ADC, HIGH); //turn off device
+  PORTB |= B00000100; //digitalWrite(CS_ADC, HIGH); //turn off device
 
   return adcvalue;
 }
@@ -99,23 +91,20 @@ int read_adc(int channel){
 /** Write to DAC */
 void write_dac(int data) {
 
-fword = cmd | data;
+  // combine command and data
+  fword = cmd | data;
   
   // chip select
-  PORTB &= B11111101;
-  //digitalWrite(CS_DAC, LOW);
+  PORTB &= B11111101; //digitalWrite(CS_DAC, LOW);
   
   // start writing data
-  PORTB |= B00000001;
-  //digitalWrite(LDAC, HIGH);
+  PORTB |= B00000001; //digitalWrite(LDAC, HIGH);
 
   for (int i1=0; i1<16; i1++) {
     if (fword & 0x8000) {
-      PORTB |= B00001000;
-      //digitalWrite(DATAOUT, HIGH);
+      PORTB |= B00001000; //digitalWrite(DATAOUT, HIGH);
     } else {
-      PORTB &= B11110111;
-      //digitalWrite(DATAOUT, LOW);
+      PORTB &= B11110111; //digitalWrite(DATAOUT, LOW);
     }
 
     cycle_clock();
@@ -124,25 +113,38 @@ fword = cmd | data;
   }
 
   // end of conversion
-  PORTB |= B00000010;
-  //digitalWrite(CS_DAC, HIGH); 
+  PORTB |= B00000010; //digitalWrite(CS_DAC, HIGH); 
   
-  PORTB &= B11111110;
-  //digitalWrite(LDAC, LOW); // writing data to output buffer
+  // writing data to output buffer
+  PORTB &= B11111110; //digitalWrite(LDAC, LOW); 
 }
 
 bool led_on = false;
 
+int adj_counter = 0;
+
+int incr = 1;
+
+int audio_in;
+
 void loop() {
 
+  if (++adj_counter == 100) {
+    int pot = read_adc(2);
+    incr = pot / 100.0;
+    adj_counter = 0;
+
+  } else {
+    // sample audio input
+    audio_in = read_adc(1);
+  }
+
   // get next point in sine wave table
-  if (++index == NUM_SINE_WAVE_POINTS)  {
-    index = 0;
+  index += incr;
+  if (index >= NUM_SINE_WAVE_POINTS)  {
+    index -= NUM_SINE_WAVE_POINTS;
   }
   data = sineWave[index];
-
-  // sample audio input
-  int audio_in = read_adc(1);
 
   // mix audio with sine wave
   audio_in = 2047 + ((audio_in-2047) * ((data-127) / 127.0));
@@ -156,6 +158,7 @@ void loop() {
 
   // blink the LED after every n samples (it just wouldn't be 
   // an arduino sketch without a blinking LED)
+/*
   if (++counter==n) {
     led_on = !led_on;
     if (led_on) {
@@ -167,6 +170,7 @@ void loop() {
     }
     counter = 0;
   }
+  */
 }
 
 void fill_sinewave(){
