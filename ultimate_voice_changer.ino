b14980bb17daaf76a7a3396dcf43e6b0aa21f7fb
Andy Grove <andygrove73@gmail.com>
2014-10-12 11:04:23 -0600

    remove unused macros

--- ultimate_voice_changer.ino
+++ ultimate_voice_changer.ino
@@ -7,14 +7,6 @@
  * http://theotherandygrove.com
  */
 
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif 
-
-
 const int LED = 6;
 
 // PORTB pin assignments
